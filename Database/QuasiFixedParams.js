e2bfd3dbe80fa587dab9a6b16677172f103fde85
Emilio Islas <milio11@yahoo.com>
2015-11-02 17:49:07 -0800

    Update QuasiFixedParams.js
    - Extended to Weapons and Armors.
    - Added new parameters (MCC, TCC, PDC, MDC, FDC, EXC)

--- Database/QuasiFixedParams.js
+++ Database/QuasiFixedParams.js
@@ -1,7 +1,7 @@
 //=============================================================================
 // Quasi Fixed Params
-// Version: 1.01
-// Last Update: November 1, 2015
+// Version: 1.02
+// Last Update: November 2, 2015
 //=============================================================================
 // ** Terms of Use
 // http://quasixi.com/mv/
@@ -11,32 +11,41 @@
 //  - Save this file as "QuasiFixedParams.js" in your js/plugins/ folder
 //  - Add plugin through the plugin manager
 //  - Open the Help menu for setup guide or visit one of the following:
-//  - - http://quasixi.com/mv/
+//  - - http://quasixi.com/mv/fixed-params/
 //  - - http://forums.rpgmakerweb.com/index.php?/topic/48777-quasi-fixed-params/
 //=============================================================================
 var Imported = Imported || {};
-Imported.Quasi_FixedParams = 1.01;
+Imported.Quasi_FixedParams = 1.02;
 //=============================================================================
  /*:
- * @plugindesc Change the way movement works.
+ * @plugindesc Adds some new parameters related to Sp-Param and Ex-Param
  * @author Quasi      Site: http://quasixi.com
  *
  * @help
  * =============================================================================
- * ** Setting up a State with Fixed Params
+ * ** Setting up a States or Equips with Fixed Params
  * =============================================================================
- * Fixed Params allows you to add a fixed param value to states. With this you
- * can create states that will add a constant value to a param. Or create
- * a posion / regeneration state that ticks a fixed number.
+ * Fixed Params allows you to add a fixed param value to states and equipment.
+ * With this you can create states that will add a constant value to a param.
+ * Or create a posion / regeneration state that ticks a fixed number.
  *   Setting up <note tag>
  *       <params>
  *       Param: Value
  *       </params>
- *     Where param can be: MHP, MMP, ATK, DEF, MAT, MDF, AGI, LUK, HRT, MRT, TRT
+ *     Where param can be: MHP, MMP, ATK, DEF, MAT, MDF, AGI, LUK and new Params
  *     Value can be any number or even a formula. Can use a and v[] but not b!
  *
- * HRT, MRT, TRT are new params that work like HRG, MRG, TGR but instead of
- * increasing/decreasing by a percentage, they tick a fixed value.
+ *   New Params:
+ *   (HRT) Hp Regeneration tick     - Works like Hp Regeneration (Ex-Parameter)
+ *   (MRT) Mp Regeneration tick     - Works like Mp Regeneration (Ex-Parameter)
+ *   (TRT) Tp Regeneration tick     - Works like Tp Regeneration (Ex-Parameter)
+ *   (MCC) Mp Cost Constant         - Works like Mp Cost Rate (Sp-Parameter)
+ *   (TCC) Tp Charge Constant       - Works like Tp Charge Rate (Sp-Parameter)
+ *   (PDC) Physical Damage Constant - Works like Physical Damage (Sp-Parameter)
+ *   (MDC) Magical Damage Constant  - Works like Magical Damage (Sp-Parameter)
+ *   (FDC) Floor Damage Constant    - Works like Floor Damage (Sp-Parameter)
+ *   (EXC) Experience Constant      - Works like Expericence (Sp-Parameter)
+ * The difference is that these new stats are fixed values, not a percent.
  * =============================================================================
  * ** Examples:
  * =============================================================================
@@ -66,9 +75,11 @@ Imported.Quasi_FixedParams = 1.01;
 (function() {
   Params = {};
   Params.id = {
-    "mhp": 0, "mmp": 1, "atk": 2, "def": 3,
-    "mat": 4, "mdf": 5, "agi": 6, "luk": 7,
-    "hrt": 8, "mrt": 9, "trt": 10
+    "mhp": 0,  "mmp": 1,  "atk": 2,  "def": 3,
+    "mat": 4,  "mdf": 5,  "agi": 6,  "luk": 7,
+    "hrt": 8,  "mrt": 9,  "trt": 10, "mcc": 11,
+    "tcc": 12, "pdc": 13, "mdc": 14, "fdc": 15,
+    "exc": 16
   }
   Params.states = {};
   Params.stateParamsPlus = function(stateId) {
@@ -81,6 +92,28 @@ Imported.Quasi_FixedParams = 1.01;
       }
     }
     return this.states[stateId];
+  };
+
+  Params.equips = {};
+  Params.equips[0] = {};
+  Params.equips[1] = {};
+  Params.equipParamsPlus = function(equipId, isWeapon) {
+    if (isWeapon) {
+      var data = this.equips[0];
+      var note = $dataWeapons[equipId].note;
+    } else {
+      var data = this.equips[1];
+      var note = $dataArmors[equipId].note;
+    }
+    if (!data[equipId]) {
+      var params = /<params>([\s\S]*)<\/params>/i.exec(note);
+      if (params) {
+        data[equipId] = stringToObjAry(params[0]);
+      } else {
+        data[equipId] = false;
+      }
+    }
+    return data[equipId];
   };
 
   function stringToObjAry(string) {
@@ -108,13 +141,26 @@ Imported.Quasi_FixedParams = 1.01;
     // Mp Regeneration tick
     mrt: { get: function() { return this.qParam(1); }, configurable: true },
     // Tp Regeneration tick
-    trt: { get: function() { return this.qParam(2); }, configurable: true }
+    trt: { get: function() { return this.qParam(2); }, configurable: true },
+    // Mp Cost Constant
+    mcc: { get: function() { return this.qParam(3); }, configurable: true },
+    // Tp Charge Constant
+    tcc: { get: function() { return this.qParam(4); }, configurable: true },
+    // Physical Damage Constant
+    pdc: { get: function() { return this.qParam(5); }, configurable: true },
+    // Magical Damage Constant
+    mdc: { get: function() { return this.qParam(6); }, configurable: true },
+    // Floor Damage Constant
+    fdc: { get: function() { return this.qParam(7); }, configurable: true },
+    // EXperience Constant
+    exc: { get: function() { return this.qParam(8); }, configurable: true }
   });
 
   var Alias_Game_BattlerBase_param = Game_BattlerBase.prototype.param;
   Game_BattlerBase.prototype.param = function(paramId) {
     var value = Alias_Game_BattlerBase_param.call(this, paramId);
     value += this.stateParamPlus(paramId);
+    value += this.equipParamPlus(paramId);
     var maxValue = this.paramMax(paramId);
     var minValue = this.paramMin(paramId);
     return Math.round(value.clamp(minValue, maxValue));
@@ -134,8 +180,18 @@ Imported.Quasi_FixedParams = 1.01;
     return value;
   };
 
+  Game_BattlerBase.prototype.equipParamPlus = function(paramId) {
+    return 0;
+  };
+
   Game_BattlerBase.prototype.qParam = function(qParamId) {
-    return this.stateParamPlus(qParamId + 8);
+    return Number(this.stateParamPlus(qParamId + 8) + this.equipParamPlus(qParamId + 8) || 0);
+  };
+
+  var Alias_Game_BattlerBase_skillMpCost = Game_BattlerBase.prototype.skillMpCost;
+  Game_BattlerBase.prototype.skillMpCost = function(skill) {
+    var value = Alias_Game_BattlerBase_skillMpCost.call(this, skill);
+    return Math.floor(value + this.mcc);
   };
 
   //-----------------------------------------------------------------------------
@@ -162,5 +218,77 @@ Imported.Quasi_FixedParams = 1.01;
   Game_Battler.prototype.regenerateTp = function() {
     var value = Math.floor(100 * this.trg + this.trt);
     this.gainSilentTp(value);
+  };
+
+  Game_Battler.prototype.chargeTpByDamage = function(damageRate) {
+    var value = Math.floor(50 * damageRate * this.tcr + this.tcc);
+    this.gainSilentTp(value);
+  };
+
+  //-----------------------------------------------------------------------------
+  // Game_Actor
+  //
+  // The game object class for an actor.
+
+  Game_Actor.prototype.equipParamPlus = function(paramId) {
+    var value = 0;
+    var equips = this.equips();
+    equips.forEach(function(equip) {
+      if (equip) {
+        var params = Params.equipParamsPlus(equip.id, DataManager.isWeapon(equip));
+        if (params[paramId]) {
+          var v = $gameVariables._data;
+          var a = this;
+          value += eval(params[paramId]);
+        }
+      }
+    }, this);
+    return value;
+  };
+
+  var Alias_Game_Actor_basicFloorDamage = Game_Actor.prototype.basicFloorDamage;
+  Game_Actor.prototype.basicFloorDamage = function() {
+    var value = Alias_Game_Actor_basicFloorDamage.call(this);
+    return value + this.fdc;
+  };
+
+  var Alias_Game_Actor_finalExpRate = Game_Actor.prototype.finalExpRate;
+  Game_Actor.prototype.finalExpRate = function() {
+    var value = Alias_Game_Actor_finalExpRate.call(this);
+    return value + this.exc;
+  };
+
+  //-----------------------------------------------------------------------------
+  // Game_Action
+  //
+  // The game object class for a battle action.
+
+  Game_Action.prototype.makeDamageValue = function(target, critical) {
+    var item = this.item();
+    var baseValue = this.evalDamageFormula(target);
+    var value = baseValue * this.calcElementRate(target);
+    if (this.isPhysical()) {
+      value *= target.pdr;
+      value += target.pdc;
+    }
+    if (this.isMagical()) {
+      value *= target.mdr;
+      value += target.mdc;
+    }
+    if (baseValue < 0) {
+      value *= target.rec;
+    }
+    if (critical) {
+      value = this.applyCritical(value);
+    }
+    value = this.applyVariance(value, item.damage.variance);
+    value = this.applyGuard(value, target);
+    value = Math.round(value);
+    return value;
+  };
+
+  Game_Action.prototype.applyItemUserEffect = function(target) {
+    var value = Math.floor(this.item().tpGain * this.subject().tcr + this.subject().tcc);
+    this.subject().gainSilentTp(value);
   };
 })();
