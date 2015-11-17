948a61078ec55ddba81013824c10cf97ed81124d
Emilio Islas <milio11@yahoo.com>
2015-11-16 21:12:33 -0800

    Update QuasiPathfind.js
    Added some extra calls for better usage. Plus optimization option.

--- Systems/QuasiPathfind.js
+++ Systems/QuasiPathfind.js
@@ -1,7 +1,7 @@
 //============================================================================
 // Quasi Pathfind
-// Version: 1.01
-// Last Update: November 8, 2015
+// Version: 1.02
+// Last Update: November 15, 2015
 //============================================================================
 // ** Terms of Use
 // http://quasixi.com/mv/
@@ -18,7 +18,7 @@
 //============================================================================
 
 var Imported = Imported || {};
-Imported.Quasi_PathFind = 1.01;
+Imported.Quasi_PathFind = 1.02;
 
 //=============================================================================
  /*:
@@ -35,10 +35,15 @@ Imported.Quasi_PathFind = 1.01;
  * default: true
  * @default true
  *
+ * @param Optimize Move Tiles
+ * @desc Setting this to true will make pathfinding faster but less accurate
+ * if you are doing pixel based.
+ * @default true
+ *
  * @param Show Console Logs
  * @desc Shows logs about the path finding and time taken.
- * default: true
- * @default true
+ * default: false
+ * @default false
  *
  * @help
  * =============================================================================
@@ -46,11 +51,35 @@ Imported.Quasi_PathFind = 1.01;
  * =============================================================================
  * By default, the player will pathfind to the location on the map that you
  * click on. But you can also move the player or events with a script call.
- *   Force Pathfind <Script Call>
- *       $gamePlayer.pathFind(x, y)
+ *   Force Pathfind <Script Call> (pixel based)
+ *       $gamePlayer.pathFind(pixelX, pixelY)
  *           or
- *       $gameMap.event(ID).pathFind(x, y)
- *     Set x and y to the location you want to reach, in pixel coordinates.
+ *       $gameMap.event(ID).pathFind(pixelX, pixelY)
+ *     Set pixelX and pixelY to the location you want to reach, in pixel coordinates.
+ *
+ *   Force Pathfind <Script Call> (grid based)
+ *       $gamePlayer.pathFindGrid(x, y)
+ *           or
+ *       $gameMap.event(ID).pathFindGrid(x, y)
+ *     Set x and y to the location you want to reach, in grid coordinates.
+ *
+ *   Pathfind towards a character <Script Call>
+ *       $gamePlayer.pathFindTowards(CharaID)
+ *           or
+ *       $gameMap.event(ID).pathFindGrid(CharaID)
+ *     Set CharaID to the ID of the event you want to scroll towards.
+ *      - If CharaID is set to 0, then it will scroll to the player.
+ * =============================================================================
+ * ** Move Route
+ * =============================================================================
+ * * The following are to be placed inside "Script..." commands
+ *  Script calls for Move Routes:
+ *       pathFind(pixelX, pixelY)
+ *         and
+ *       pathFindGrid(x, y)
+ *         and
+ *       pathFindTowards(CharaID)
+ *     Same as the script call settings above.
  * =============================================================================
  * Links
  *  - http://quasixi.com/mv/
@@ -70,6 +99,7 @@ if (!Imported.Quasi_Movement) {
     this.searchLimit = Number(parameters['Search Limit'] || 2);
     this.moveOnClick = parameters['Pathfind on Click'].toLowerCase() === "true";
     this.showLog     = parameters['Show Console Logs'].toLowerCase() === "true";
+    this.optTiles    = parameters['Optimize Move Tiles'].toLowerCase() === "true";
   };
   Pathfind.proccessParameters();
 
@@ -86,8 +116,8 @@ if (!Imported.Quasi_Movement) {
     return node;
   };
 
-  Pathfind.nodeDistance = function(inital, final) {
-    return Math.abs(inital.x - final.x) + Math.abs(inital.y - final.y);
+  Pathfind.nodeDistance = function(initial, final) {
+    return Math.abs(initial.x - final.x) + Math.abs(initial.y - final.y);
   };
 
   //-----------------------------------------------------------------------------
@@ -95,9 +125,37 @@ if (!Imported.Quasi_Movement) {
   //
   // The superclass of Game_Player, Game_Follower, GameVehicle, and Game_Event.
 
+  var Alias_Game_Character_processMoveCommand = Game_Character.prototype.processMoveCommand;
+  Game_Character.prototype.processMoveCommand = function(command) {
+    var gc = Game_Character;
+    var params = command.parameters;
+    var force = this === $gamePlayer ? true : false;
+    if (command.code === gc.ROUTE_SCRIPT) {
+      var find  = /pathFind\((.*)\)/i.exec(params[0]);
+      var findG = /pathFindGrid\((.*)\)/i.exec(params[0]);
+      var findC = /pathFindTowards\((.*)\)/i.exec(params[0]);
+      if (find) {
+        find = QuasiMovement.stringToAry(find[1]);
+        this.pathFindGrid(find[0], find[1], force);
+        return;
+      }
+      if (findG) {
+        findG = QuasiMovement.stringToAry(findG[1]);
+        this.pathFindGrid(findG[0], findG[1], force);
+        return;
+      }
+      if (findC) {
+        findC = QuasiMovement.stringToAry(findC[1]);
+        this.pathFindTowards(findC[0], force);
+        return;
+      }
+    }
+    Alias_Game_Character_processMoveCommand.call(this, command);
+  };
+
   Game_Character.prototype.updatePathFind = function(goalX, goalY) {
-    if (this._pathFind) {
-      if (this._pathFind.length === 1) {
+    if (this._pathFind && this._pathFind.path) {
+      if (this._pathFind.path.length === 1) {
         this._pathFind = null;
         $gameTemp.clearDestination();
         return;
@@ -107,18 +165,19 @@ if (!Imported.Quasi_Movement) {
         this._pathFind = null;
         return;
       }
-      var dir;
-      var current = this._pathFind.shift();
-      var next = this._pathFind[0];
+      var dir, dist;
+      var current = this._pathFind.path.shift();
+      var next = this._pathFind.path[0];
       var sx = current[0] - next[0];
       var sy = current[1] - next[1];
-      var dir;
       if (Math.abs(sx) > Math.abs(sy)) {
-        dir = sx > 0 ? 4 : 6;
+        dir  = sx > 0 ? 4 : 6;
+        dist = Math.abs(sx);
       } else if (sy !== 0) {
-        dir = sy > 0 ? 8 : 2;
+        dir  = sy > 0 ? 8 : 2;
+        dist = Math.abs(sy);
       }
-      this.moveStraight(dir);
+      this.fixedMove(dir, dist);
     }
   };
 
@@ -130,8 +189,37 @@ if (!Imported.Quasi_Movement) {
     }
   };
 
-  Game_Character.prototype.pathFind = function(goalX, goalY) {
-    this._pathFind = null;
+  Game_Character.prototype.pathFindGrid = function(goalX, goalY, forced) {
+    var x = goalX * QuasiMovement.tileSize;
+    var y = goalY * QuasiMovement.tileSize;
+    this.pathFind(x, y, forced);
+  };
+
+  Game_Character.prototype.pathFindTowards = function(chara, forced) {
+    if (chara === 0) {
+      chara = $gamePlayer;
+    } else {
+      chara = $gameMap.event(chara);
+    }
+    if (this === chara) {
+      return;
+    }
+    var x = chara._px;
+    var y = chara._py;
+    var padding = [];
+    padding[0] = Math.abs(this.collider().width - chara.collider().width);
+    padding[0] += this.moveTiles();
+    padding[1] = Math.abs(this.collider().height - chara.collider().height);
+    padding[1] += this.moveTiles();
+    this.pathFind(x, y, forced, padding);
+  };
+
+  Game_Character.prototype.pathFind = function(goalX, goalY, forced, padding) {
+    this._pathFind = {};
+    this._pathFind.path = null;
+    padding = padding || [0, 0];
+    this._pathFind.paddingX = padding[0];
+    this._pathFind.paddingY = padding[1]
     if (!QuasiMovement.offGrid) {
       var ox = goalX % this.moveTiles();
       var oy = goalY % this.moveTiles();
@@ -140,22 +228,89 @@ if (!Imported.Quasi_Movement) {
     }
     if (!this.canPass(goalX, goalY, 5)) {
       if (Pathfind.showLog) {
-        console.log("Not passable");
+        console.log("Trying Neighboring points");
+      }
+      var neighbor = this.anyNeighborPass(goalX, goalY);
+      if (neighbor) {
+        if (Pathfind.showLog) {
+          console.log("Neighboring points found");
+        }
+        this.pathFind(neighbor[0], neighbor[1], forced, padding);
+        return;
+      } else {
+        if (Pathfind.showLog) {
+          console.log("Not passable");
+        }
+        return;
+      }
+    }
+    if (!this.canPass(this._px, this._py, 5)) {
+      if (Pathfind.showLog) {
+        console.log("Can't move");
       }
       return;
     }
     if (goalX === this._px && goalY === this._py) {
       return;
     }
-    this._pathFind = this.aStar([this._px, this._py], [goalX, goalY]);
-    if (this === $gamePlayer) {
+    this._pathFind.path = this.aStar([this._px, this._py], [goalX, goalY]);
+    if (this === $gamePlayer && !forced) {
       this._tempGoal = [$gameTemp.destinationPX(), $gameTemp.destinationPY()];
+    } else {
+      this.endPathFind(typeof forced === 'undefined' ? true : forced);
+      if (this === $gamePlayer) {
+        $gameTemp.clearDestination();
+      }
+      this._pathFind = null;
     }
-    this.endPathFind();
   };
 
-  Game_Character.prototype.endPathFind = function() {
-
+  Game_Character.prototype.endPathFind = function(forced) {
+    var move = {
+      2: Game_Character.ROUTE_MOVE_DOWN,     4: Game_Character.ROUTE_MOVE_LEFT,
+      6: Game_Character.ROUTE_MOVE_RIGHT,    8: Game_Character.ROUTE_MOVE_UP
+    }
+    if (forced) {
+      var route = {};
+      route.list = [];
+      route.repeat = false;
+      route.skippable = false;
+      route.wait = false;
+    } else {
+      var route = this._moveRoute;
+    }
+    var dir;
+    var current = this._pathFind.path.shift();
+    var i, j, k, sx, sy, command, dist;
+    for (i = 0, j = this._pathFind.path.length; i < j; i++) {
+     sx = current[0] - this._pathFind.path[i][0];
+     sy = current[1] - this._pathFind.path[i][1];
+     if (Math.abs(sx) > Math.abs(sy)) {
+       dir = sx > 0 ? 4 : 6;
+       dist = Math.abs(sx) / this.moveTiles();
+     } else if (sy !== 0) {
+       dir = sy > 0 ? 8 : 2;
+       dist = Math.abs(sy) / this.moveTiles();
+     }
+     for (k = 0; k < dist; k++) {
+       command = {};
+       command.code = move[dir];
+       if (forced) {
+         route.list.push(command);
+       } else {
+         this._moveRoute.list.splice(this._moveRouteIndex + 1 + i, 0, command);
+       }
+     }
+     current = this._pathFind.path[i];
+    }
+    if (forced) {
+     command = {};
+     command.code = 0;
+     route.list.push(command);
+     this.forceMoveRoute(route);
+    } else {
+     this._moveRoute.list.splice(this._moveRouteIndex, 1);
+    }
   };
 
   Game_Character.prototype.aStar = function(start, end) {
@@ -168,6 +323,7 @@ if (!Imported.Quasi_Movement) {
     var neighbors, newNode, finalPath;
     var max = Pathfind.searchLimit;
     var i, j;
+    var finalPath = [];
     if (Pathfind.showLog) {
       console.log("Pathfinding Started");
       console.time("Pathfind");
@@ -215,21 +371,94 @@ if (!Imported.Quasi_Movement) {
 
   Game_Character.prototype.findNeighbors = function(x, y, endNode) {
     var neighbors = [];
-    var i, dir, x2, y2;
+    var i, dir, x2, y2, successor;
+    var tiles = this.optTiles();
+    var initialOX = x % tiles;
+    var initialOY = y % tiles;
+    var nearEnd = Math.abs(x - endNode.x) < QuasiMovement.tileSize && Math.abs(y - endNode.y) < QuasiMovement.tileSize;
     for (i = 1; i < 5; i++) {
       dir = i * 2;
-      if (this.canPass(x, y, dir, this.moveTiles())) {
-        x2 = $gameMap.roundPXWithDirection(x, dir, this.moveTiles());
-        y2 = $gameMap.roundPYWithDirection(y, dir, this.moveTiles());
-        if (Math.abs(x2 - endNode.x) < this.moveTiles() &&
-            Math.abs(y2 - endNode.y) < this.moveTiles()) {
+      x2 = x3 = $gameMap.roundPXWithDirection(x, dir, tiles);
+      y2 = y3 = $gameMap.roundPYWithDirection(y, dir, tiles);
+      if (this.canPass(x, y, dir, tiles)) {
+        if (Math.abs(x2 - endNode.x) <= tiles &&
+            Math.abs(y2 - endNode.y) <= tiles) {
           neighbors.push([endNode.x, endNode.y]);
         } else {
           neighbors.push([x2, y2]);
         }
+        continue;
+      }
+      if (Pathfind.successor) {
+        successor = this.findSuccessors(x, y, x2, y2, dir, tiles);
+        if (successor) {
+          neighbors.push(successor);
+        }
       }
     }
     return neighbors;
+  };
+
+  Game_Character.prototype.findSuccessors = function(x1, y1, x2, y2, dir, tiles) {
+    if (tiles === this.moveTiles() || !Pathfind.optTiles) {
+      return;
+    }
+    var dir = this.reverseDir(dir);
+    var amt = tiles / this.moveTiles();
+    var success, i, dist;
+    for (i = 0; i < amt; i++) {
+      if (dir === 4 || dir === 6) {
+        dist = Math.abs(x1 - x2);
+      } else {
+        dist = Math.abs(y1 - y2);
+      }
+      if (this.canPass(x2, y2, 5, dist)) {
+        success = [x2, y2];
+        break;
+      }
+      x2 = $gameMap.roundPXWithDirection(x2, dir, this.moveTiles());
+      y2 = $gameMap.roundPYWithDirection(y2, dir, this.moveTiles());
+    }
+    return success;
+  };
+
+  Game_Character.prototype.optTiles = function() {
+    if (!this._optTiles) {
+      var w = this.collider().width;
+      var h = this.collider().height;
+      while (w % this.moveTiles() !== 0) {
+        w--;
+      }
+      while (h % this.moveTiles() !== 0) {
+        h--;
+      }
+      this._optTiles = Math.max(Math.min(w, h), this.moveTiles());
+    }
+    return this._optTiles;
+  };
+
+  Game_Character.prototype.anyNeighborPass = function(x, y) {
+    var maxDist, dist;
+    var maxDistX = this.collider().width + this._pathFind.paddingX;
+    var maxDistY = this.collider().height + this._pathFind.paddingY;
+    var neighbors = [];
+    for (i = 1; i < 5; i++) {
+      dir = i * 2;
+      maxDist = dir === 2 || dir === 8 ? maxDistY : maxDistX;
+      if (this.canPass(x, y, dir, maxDist)) {
+        x2 = $gameMap.roundPXWithDirection(x, dir, maxDist);
+        y2 = $gameMap.roundPYWithDirection(y, dir, maxDist);
+        dist = Math.abs(this._px - x2) + Math.abs(this._py - y2);
+        neighbors.push([x2, y2, dist]);
+      }
+    }
+    if (neighbors.length === 0) {
+      return false;
+    }
+    neighbors.sort(function(a, b) {
+      return a[2] - b[2];
+    });
+    return neighbors[0];
   };
 
   Game_Character.prototype.createPathFrom = function(node) {
@@ -239,38 +468,6 @@ if (!Imported.Quasi_Movement) {
       node = node.parent;
     }while(node.parent);
     return path.reverse();
-  };
-
-  Game_Event.prototype.endPathFind = function() {
-    var move = {
-      2: Game_Character.ROUTE_MOVE_DOWN,     4: Game_Character.ROUTE_MOVE_LEFT,
-      6: Game_Character.ROUTE_MOVE_RIGHT,    8: Game_Character.ROUTE_MOVE_UP
-    }
-   var route = {};
-   route.list = [];
-   route.repeat = false;
-   route.skippable = false;
-   route.wait = false;
-   var dir;
-   var current = this._pathFind.shift();
-   var i, j, sx, sy, command;
-   for (i = 0, j = this._pathFind.length; i < j; i++) {
-     sx = current[0] - this._pathFind[i][0];
-     sy = current[1] - this._pathFind[i][1];
-     if (Math.abs(sx) > Math.abs(sy)) {
-       dir = sx > 0 ? 4 : 6;
-     } else if (sy !== 0) {
-       dir = sy > 0 ? 8 : 2;
-     }
-     command = {};
-     command.code = move[dir];
-     route.list.push(command);
-     current = this._pathFind[i];
-   }
-   command = {};
-   command.code = 0;
-   route.list.push(command);
-   this.forceMoveRoute(route);
   };
 
 })();
